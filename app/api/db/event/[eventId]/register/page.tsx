b637233f3a3e6fafa28a04789ac36a0943cacc63
Dinesh <dineshkumar.software@gmail.com>
2024-05-04 08:26:56 +0530

    Add Register Route

---
+++ app/api/db/event/[eventId]/register/page.tsx
@@ -0,0 +1,80 @@
+import { getUserUID } from "@/firebase/firebaseAdmin";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(
+    request: Request,
+    { params }: { params: { eventId: string } }
+) {
+    try {
+        const body = (await request.json()) as {
+            accessToken: string;
+        };
+        const prisma = new PrismaClient();
+
+        const uid = await getUserUID(body.accessToken);
+
+        if (!uid) {
+            return new NextResponse(
+                JSON.stringify({
+                    error: "User not found!",
+                }),
+                { status: 404 }
+            );
+        }
+
+        const user = await prisma.user.findFirst({
+            where: {
+                google_uid: uid,
+            },
+        });
+
+        if (!user) {
+            return new NextResponse(
+                JSON.stringify({
+                    error: "User not found!",
+                }),
+                { status: 404 }
+            );
+        }
+
+        const event = await prisma.event.findFirst({
+            where: {
+                id: parseInt(params.eventId as string),
+            },
+        });
+
+        if (!event) {
+            return new NextResponse(
+                JSON.stringify({
+                    error: "Event not found!",
+                }),
+                { status: 404 }
+            );
+        }
+
+        return new NextResponse(
+            JSON.stringify({
+                message: "User Registered Successfully!",
+                event: event,
+            }),
+            { status: 200 }
+        );
+    } catch (error) {
+        console.log(error);
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            return new NextResponse(
+                JSON.stringify({
+                    error: "Already Registered",
+                }),
+                { status: 409 }
+            );
+        }
+        return new NextResponse(
+            JSON.stringify({
+                error: "Internal Server Error",
+            }),
+            { status: 500 }
+        );
+    }
+}
