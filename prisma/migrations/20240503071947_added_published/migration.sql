071be265070f942e33559898657de7ed7f131934
Dinesh <dineshkumar.software@gmail.com>
2024-05-04 12:44:32 +0530

    New DB

--- prisma/migrations/20240503071947_added_published/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Blog" ADD COLUMN     "published" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "published" BOOLEAN NOT NULL DEFAULT false;
