c1bbeb2ccab75385a6727cb9cc79f4e438bb4089
Dinesh <dineshkumar.software@gmail.com>
2024-05-07 12:33:20 +0530

    chore: Refactor login route to return a static response

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -24,44 +24,45 @@ async function checkIfUserHasAccess(token: string) {
 }
 
 export async function POST(request: Request) {
-    try {
-        const token = verifyHeaderHasToken(request);
+    return new NextResponse("hello", { status: 200 });
+    // try {
+    //     const token = verifyHeaderHasToken(request);
 
-        const userDetails = await checkIfUserHasAccess(token)
-            .then((user) => {
-                return user;
-            })
-            .catch((error) => {
-                return undefined;
-            });
-        if (!userDetails) {
-            return new NextResponse(
-                JSON.stringify({
-                    not_registered: true,
-                }),
-                { status: 200 }
-            );
-        }
-        return new NextResponse(
-            JSON.stringify({
-                user: {
-                    accessToken: token,
-                    email: userDetails.email,
-                    name: userDetails.name,
-                    username: userDetails.user_name,
-                    phone: userDetails.phone,
-                    college: userDetails.college_name,
-                    avatar_url: userDetails.avatar_url,
-                    bio: userDetails.bio,
-                    not_registered: false,
-                },
-                message: "Logged In",
-            }),
-            { status: 200 }
-        );
-    } catch (error: any) {
-        return new NextResponse("You don't have access to this website", {
-            status: 401,
-        });
-    }
+    //     const userDetails = await checkIfUserHasAccess(token)
+    //         .then((user) => {
+    //             return user;
+    //         })
+    //         .catch((error) => {
+    //             return undefined;
+    //         });
+    //     if (!userDetails) {
+    //         return new NextResponse(
+    //             JSON.stringify({
+    //                 not_registered: true,
+    //             }),
+    //             { status: 200 }
+    //         );
+    //     }
+    //     return new NextResponse(
+    //         JSON.stringify({
+    //             user: {
+    //                 accessToken: token,
+    //                 email: userDetails.email,
+    //                 name: userDetails.name,
+    //                 username: userDetails.user_name,
+    //                 phone: userDetails.phone,
+    //                 college: userDetails.college_name,
+    //                 avatar_url: userDetails.avatar_url,
+    //                 bio: userDetails.bio,
+    //                 not_registered: false,
+    //             },
+    //             message: "Logged In",
+    //         }),
+    //         { status: 200 }
+    //     );
+    // } catch (error: any) {
+    //     return new NextResponse("You don't have access to this website", {
+    //         status: 401,
+    //     });
+    // }
 }
