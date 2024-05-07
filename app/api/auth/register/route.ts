4df1586b0e20e910245b6b0dd1544863b75b30e9
Dinesh <dineshkumar.software@gmail.com>
2024-05-07 14:14:19 +0530

    chore: Refactor login route to use Response instead of NextResponse and return JSON error message

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -4,76 +4,76 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 export async function POST(request: Request) {
-    // try {
-    //     const body = (await request.json()) as {
-    //         name: string;
-    //         username: string;
-    //         email: string;
-    //         phone: string;
-    //         college: string;
-    //         accessToken: string;
-    //     };
-    //     console.log(body);
-    //     const prisma = new PrismaClient();
+    try {
+        const body = (await request.json()) as {
+            name: string;
+            username: string;
+            email: string;
+            phone: string;
+            college: string;
+            accessToken: string;
+        };
+        console.log(body);
+        const prisma = new PrismaClient();
 
-    //     const uid = await getUserUID(body.accessToken);
+        const uid = await getUserUID(body.accessToken);
 
-    //     if (!uid) {
-    //         return new NextResponse(
-    //             JSON.stringify({
-    //                 error: "User not found!",
-    //             }),
-    //             { status: 404 }
-    //         );
-    //     }
+        if (!uid) {
+            return new NextResponse(
+                JSON.stringify({
+                    error: "User not found!",
+                }),
+                { status: 404 }
+            );
+        }
 
-    //     const user = await prisma.user.create({
-    //         data: {
-    //             name: body.name,
-    //             user_name: body.username,
-    //             email: body.email,
-    //             phone: body.phone,
-    //             college_name: body.college,
-    //             avatar_url: "",
-    //             google_uid: uid,
-    //             bio: "",
-    //         },
-    //     });
+        const user = await prisma.user.create({
+            data: {
+                name: body.name,
+                user_name: body.username,
+                email: body.email,
+                phone: body.phone,
+                college_name: body.college,
+                avatar_url: "",
+                google_uid: uid,
+                bio: "",
+            },
+        });
 
-    //     return new NextResponse(
-    //         JSON.stringify({
-    //             message: "User Registered Successfully!",
-    //             user: {
-    //                 accessToken: body.accessToken,
-    //                 email: user.email,
-    //                 name: user.name,
-    //                 username: user.user_name,
-    //                 phone: user.phone,
-    //                 college: user.college_name,
-    //                 avatar_url: user.avatar_url,
-    //                 bio: user.bio,
-    //                 not_registered: false,
-    //             },
-    //         }),
-    //         { status: 200 }
-    //     );
-    // } catch (error) {
-    //     console.log(error);
-    //     if (error instanceof Prisma.PrismaClientKnownRequestError) {
-    //         return new NextResponse(
-    //             JSON.stringify({
-    //                 error: "Username already exists!",
-    //             }),
-    //             { status: 409 }
-    //         );
-    //     }
-    //     return new NextResponse(
-    //         JSON.stringify({
-    //             error: "Failed Registering User!",
-    //         }),
-    //         { status: 500 }
-    //     );
-    // }
+        return new NextResponse(
+            JSON.stringify({
+                message: "User Registered Successfully!",
+                user: {
+                    accessToken: body.accessToken,
+                    email: user.email,
+                    name: user.name,
+                    username: user.user_name,
+                    phone: user.phone,
+                    college: user.college_name,
+                    avatar_url: user.avatar_url,
+                    bio: user.bio,
+                    not_registered: false,
+                },
+            }),
+            { status: 200 }
+        );
+    } catch (error) {
+        console.log(error);
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            return new NextResponse(
+                JSON.stringify({
+                    error: "Username already exists!",
+                }),
+                { status: 409 }
+            );
+        }
+        return new NextResponse(
+            JSON.stringify({
+                error: "Failed Registering User!",
+            }),
+            { status: 500 }
+        );
+    }
     return new Response("You don't have access to this website", {
         status: 401,
     });
