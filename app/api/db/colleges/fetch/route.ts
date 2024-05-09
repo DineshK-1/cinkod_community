f899222215a84d238510819404a7fe04ddc22e99
Dinesh <dineshkumar.software@gmail.com>
2024-05-09 20:42:37 +0530

    chore: Update college fetch route to use PrismaClient

--- app/api/db/colleges/fetch/route.ts
+++ app/api/db/colleges/fetch/route.ts
@@ -3,14 +3,14 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 export async function GET(request: Request) {
+    const prisma = new PrismaClient();
     try {
-        const prisma = new PrismaClient();
         const colleges = await prisma.college.findMany();
         return NextResponse.json(colleges, { status: 200 });
     } catch (e) {
         console.error(e);
         return NextResponse.json(
-            { error: "Error fetching colleges", e: e },
+            { error: "Error fetching colleges", e: e, prisma: prisma },
             { status: 500 }
         );
     }
