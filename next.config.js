7ec8e39aaac8c66a1e061e58268438ef2a0ba6fa
Dinesh <dineshkumar.software@gmail.com>
2024-05-13 15:22:57 +0530

    Added Admin key

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ module.exports = {
     env: {
         DATABASE_URL: process.env.DATABASE_URL,
         NEXT_PUBLIC_HOST_URL: process.env.NEXT_PUBLIC_HOST_URL,
+        FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
     },
 };
